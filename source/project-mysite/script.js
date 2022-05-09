65c47d6deb8d2e20e6ebde5e3634ebada46dc6dd
Andrey Surkov <68987883+Suren73@users.noreply.github.com>
2022-05-09 23:42:17 +0300

    change index.html

---
+++ source/project-mysite/script.js
@@ -0,0 +1,3 @@
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+themeButton.addEventListener("click", () => page.classList.toggle('dark-theme'));
